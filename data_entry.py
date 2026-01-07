17093a839bd807c80a9e9830d112d85a9f25a06f
Afolabi <har4labi.anim@gmail.com>
2026-01-07 14:17:11 +0100

    add data function to finance csv

--- data_entry.py
+++ data_entry.py
@@ -4,13 +4,29 @@ from datetime import datetime
 
 class CSV:
     CSV_FILE = "finance_data.csv"
+    COLUMNS = ["date","amount","category","description"]
 
     @classmethod
     def initialize_csv(cls):
         try:
             pd.read_csv(cls.CSV_FILE)
         except FileNotFoundError:
-            finance_df = pd.DataFrame(columns=["date","amount","category","description"])
+            finance_df = pd.DataFrame(columns=cls.COLUMNS)
             finance_df.to_csv(cls.CSV_FILE, index=False)
 
+    @classmethod
+    def add_data(cls,date,amount,category,description):
+        new_data = {
+            "date":date,
+            "amount":amount,
+            "category":category,
+            "description":description
+        }
+
+        with open(cls.CSV_FILE, mode="a", newline="") as finance_csv:
+            csv_write = csv.DictWriter(finance_csv, fieldnames=cls.COLUMNS)
+            csv_write.writerow(new_data)
+        print("Data added successfully!")
+
 CSV.initialize_csv()
+CSV.add_data("07-01-2026",67000.00,"Income","Jewelry")
